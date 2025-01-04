53104db56e2392794ef40bbd128e03eed5355536
Nikos Sklikas <nikos004@gmail.com>
2025-01-04 16:36:57 +0200

    fix portals

--- portal.gd
+++ portal.gd
@@ -2,11 +2,17 @@ extends Area3D
 
 
 @export var connect_portal: Area3D
+var teleported_to: bool = false
 var offset = Vector3(0 , 0 , 3)
 
 
 func _on_body_entered(body: Node3D) -> void:
-	if body.name == "Bob":
+	if body.name == "Bob" && not teleported_to:
 		var destination = connect_portal.global_transform.origin
-		body.global_transform.origin = destination + offset
+		connect_portal.teleported_to = true
+		body.global_transform.origin = destination
 		
+
+
+func _on_body_exited(body: Node3D) -> void:
+	teleported_to = false
